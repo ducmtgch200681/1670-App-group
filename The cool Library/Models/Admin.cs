642319a0143aa5ddfbf40f85adeab0d745f73daa
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-10-20 05:22:10 +0700

    ok

--- The cool Library/Models/Admin.cs
+++ The cool Library/Models/Admin.cs
@@ -9,19 +9,10 @@ namespace The_cool_Library.Models
         //[Key]
         //public int Admin_id { get; set; }
 
-        [Required]
-        [MinLength(5, ErrorMessage = "Fullname must be at least 5 characters !"),
-         MaxLength(30, ErrorMessage = "Fullname maximum is 30 characters !")]
+        [MinLength(5, ErrorMessage = "Fullname must be at least 5 characters !")]
+        [MaxLength(30, ErrorMessage = "Fullname maximum is 30 characters !")]
         public string Ad_name { get; set; }
 
-        [Required]
-        [EmailAddress]
-        public string Ad_email { get; set; }
-
-        [Required]
-        [MinLength(5, ErrorMessage = "Password must be at least 5 characters !"),
-         MaxLength(15, ErrorMessage = "Password maximum is 15 characters !")]
-        public string Ad_pass { get; set; }
     }
 }
     
